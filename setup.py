c16a76c0f9e4f6687439af0179fa9a76dbfc9e6f
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-08-19 12:19:12 +0530

    setup.py completed

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md','r',encoding='utf-8') as f:
+    long_description=f.read()
+
+__version__='0.0.0'
+
+REPO_NAME='Drinks-Quality-Prediction-System'
+AUTHOR_USER_NAME='mdzaheerjk'
+SRC_REPO='mlProject'
+AUTHOR_EMAIL='zaheerjkxai@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    desciption="A Small python package for ml app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/mdzaheerjk/Drinks-Quality-Prediction-System",
+    project_urls={
+        "Bug Tracker":f"https://github.com/mdzaheerjk/Drinks-Quality-Prediction-System/issues"
+
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where='src')
+
+)
