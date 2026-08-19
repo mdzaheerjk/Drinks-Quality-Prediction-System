60cb02d3fdc8ee4570eaf6e4e099f39c053f5235
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-08-19 18:21:15 +0530

    components

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,24 @@
+import os
+from mlProject import logger
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from mlProject.entity.config_entity import DataTransformatinConfig
+
+class DataTransformation:
+    def __init__(self,config:DataTransformatinConfig):
+        self.config=config
+
+    def train_test_spliting(self):
+        data=pd.read_csv(self.config.data_path)
+
+        train,test=train_test_split(data)
+
+        train.to_csv(os.path.join(self.config.root_dir,"train.csv"),index=False)
+        test.to_csv(os.path.join(self.config.root_dir,"test.csv"),index=False)
+
+        logger.info("Splitted data into training and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
